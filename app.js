a06d84132349f6f4f578fc614e2a0a7f6e0d78bd
yenytm <143008413+yenytm@users.noreply.github.com>
2023-10-17 00:55:19 +0000

    fix NaN error

--- app.js
+++ app.js
@@ -9,27 +9,27 @@ let oneEuroIs = {
             console.log("You can't have a negative value")
             return 
         }
-        return +(euro * 1.2).toFixed(2)
+        return Number((euro * 1.2).toFixed(2))
     }
-    console.log(fromEuroToDollar ())
+    console.log(fromEuroToDollar (4))
 
     function fromDollarToYen (dollar) {
-        if(dollar < 0) {
+        if(dollar < 0 || !dollar) {
             console.log("You can't have a negative value")
             return 
         }
-        return +(dollar * 149.59).toFixed(2)
+        return Number((dollar * 149.59).toFixed(2))
     }
-    console.log(fromDollarToYen())
+    console.log(fromDollarToYen(7))
 
     function fromYenToPound (yen) {
         if(yen < 0) {
             console.log("You can't have a negative value")
             return 
         }
-        return +(yen * 0.0055).toFixed(3)
+        return Number((yen * 0.0055).toFixed(3))
     }
-    console.log(fromYenToPound())
+    console.log(fromYenToPound(99))
 
 
 module.exports = {fromEuroToDollar, fromDollarToYen, fromYenToPound};
